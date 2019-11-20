d73a3424048e3eff7d2c8e19fd4a7d86f0dce664
Ermal Kaleci <ermalkaleci@gmail.com>
2019-11-20 21:38:18 +0100

    update LiquidityPool default impl

--- modules/liquidity_pools/src/tests.rs
+++ modules/liquidity_pools/src/tests.rs
@@ -5,31 +5,31 @@ use crate::{
 	LiquidityPool,
 };
 
+use sr_primitives::Perbill;
 use support::assert_ok;
 
 #[test]
 fn should_create_pool() {
 	new_test_ext().execute_with(|| {
-		let alice = 1;
-		let bob = 2;
+		let alice: u64 = 1;
+
+		let liquidity_pool = LiquidityPool {
+			bid_spread: Perbill::zero(),
+			ask_spread: Perbill::zero(),
+			additional_collateral_ratio: None,
+		};
 
 		assert_ok!(ModuleLiquidityPools::create_pool(Origin::signed(alice)));
-		assert_eq!(ModuleLiquidityPools::liquidity_pools(1), LiquidityPool::default());
-		// pool_id 1 is owned by Alice
+		assert_eq!(ModuleLiquidityPools::liquidity_pools(1), liquidity_pool);
 		assert_eq!(ModuleLiquidityPools::owners(1), alice);
-
-		assert_ok!(ModuleLiquidityPools::create_pool(Origin::signed(bob)));
-		// pool_id 2 is owned by Bob
-		assert_eq!(ModuleLiquidityPools::owners(2), bob);
-
-		assert_eq!(ModuleLiquidityPools::next_pool_id(), 3);
+		assert_eq!(ModuleLiquidityPools::next_pool_id(), 2);
 	});
 }
 
 #[test]
 fn is_owner_should_work() {
 	new_test_ext().execute_with(|| {
-		let alice = 1;
+		let alice: u64 = 1;
 		assert_ok!(ModuleLiquidityPools::create_pool(Origin::signed(alice)));
 		assert_eq!(ModuleLiquidityPools::is_owner(1, alice), true);
 		assert_eq!(ModuleLiquidityPools::is_owner(2, alice), false);
