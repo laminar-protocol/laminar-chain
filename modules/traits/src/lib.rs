b227819c1012d12274fbb8c2788123afbe115e6f
Shaopeng Wang <spxwang@gmail.com>
2019-11-26 16:29:27 +1300

    Define LiquidityPools traits.

--- modules/traits/src/lib.rs
+++ modules/traits/src/lib.rs
@@ -1,5 +1,30 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-pub trait LiquidityPool {
-	type LiquidityPoolId;
+use codec::FullCodec;
+use num_traits::Signed;
+use rstd::fmt::Debug;
+use sr_primitives::{traits::MaybeSerializeDeserialize, Permill};
+
+pub trait LiquidityPoolBaseTypes {
+	type LiquidityPoolId: FullCodec + Eq + PartialEq + Copy + MaybeSerializeDeserialize + Debug;
+	type CurrencyId: FullCodec + Eq + PartialEq + Copy + MaybeSerializeDeserialize + Debug;
 }
+
+pub trait LiquidityPoolsConfig: LiquidityPoolBaseTypes {
+	fn get_bid_spread(pool_id: Self::LiquidityPoolId, currency_id: Self::CurrencyId) -> Permill;
+	fn get_ask_spread(pool_id: Self::LiquidityPoolId, currency_id: Self::CurrencyId) -> Permill;
+	fn get_additional_collateral_ratio(pool_id: Self::LiquidityPoolId, currency_id: Self::CurrencyId) -> Permill;
+}
+
+pub trait LiquidityPoolsPosition: LiquidityPoolBaseTypes {
+	/// Signed leverage type: positive means long and negative means short.
+	type Leverage: Signed + FullCodec + Eq + PartialEq + PartialOrd + Ord + Copy + MaybeSerializeDeserialize + Debug;
+
+	fn is_allowed_position(
+		pool_id: Self::LiquidityPoolId,
+		currency_id: Self::CurrencyId,
+		leverage: Self::Leverage,
+	) -> bool;
+}
+
+pub trait LiquidityPools: LiquidityPoolsConfig + LiquidityPoolsPosition {}
