ce3ac49d1c8108494b9e83a028c1fb733daf42e7
Shaopeng Wang <spxwang@gmail.com>
2019-12-03 17:29:01 +1300

    Mint unit tests.

--- modules/synthetic-protocol/src/mock.rs
+++ modules/synthetic-protocol/src/mock.rs
@@ -11,10 +11,12 @@ use sr_primitives::{testing::Header, traits::IdentityLookup, Perbill};
 use orml_currencies::BasicCurrencyAdapter;
 use orml_traits::DataProvider;
 
-use module_primitives::{Balance, BalancePriceConverter, CurrencyId, LiquidityPoolId};
+use module_primitives::{Balance, BalancePriceConverter, LiquidityPoolId};
 use traits::LiquidityPoolBaseTypes;
 
 use super::*;
+
+pub use module_primitives::CurrencyId;
 
 impl_outer_origin! {
 	pub enum Origin for Runtime {}
@@ -119,11 +121,43 @@ impl orml_currencies::Trait for Runtime {
 	type GetNativeCurrencyId = GetFlowTokenId;
 }
 
-// TODO: replace this mock
+pub const DEFAULT_PRICE: (Balance, Balance) = (12, 10);
+/// price = x / y
+#[derive(Debug)]
+pub struct MockPrice(Balance, Balance);
+impl MockPrice {
+	pub fn get(&self) -> Option<Price> {
+		if self.0 == 0 && self.1 == 0 {
+			None
+		} else {
+			Some(Price::from_rational(self.0, self.1))
+		}
+	}
+
+	pub fn set_price(&mut self, x: Balance, y: Balance) {
+		self.0 = x;
+		self.1 = y;
+	}
+
+	pub fn set_default(&mut self) {
+		self.0 = DEFAULT_PRICE.0;
+		self.1 = DEFAULT_PRICE.1;
+	}
+
+	pub fn set_none(&mut self) {
+		self.0 = 0;
+		self.1 = 0;
+	}
+}
+
+pub static mut MOCK_PRICE_SOURCE: MockPrice = MockPrice(DEFAULT_PRICE.0, DEFAULT_PRICE.1);
 pub struct TestSource;
 impl DataProvider<CurrencyId, Price> for TestSource {
-	fn get(_currency: &CurrencyId) -> Option<Price> {
-		None
+	fn get(currency: &CurrencyId) -> Option<Price> {
+		match currency {
+			CurrencyId::AUSD => Some(Price::from_rational(1, 1)),
+			_ => unsafe { MOCK_PRICE_SOURCE.get() },
+		}
 	}
 }
 impl orml_prices::Trait for Runtime {
@@ -139,24 +173,41 @@ impl synthetic_tokens::Trait for Runtime {
 }
 pub type SyntheticTokens = Module<Runtime>;
 
+pub const MOCK_POOL: LiquidityPoolId = 1;
+
 pub struct TestLiquidityPools<AccountId>(marker::PhantomData<AccountId>);
 impl LiquidityPoolBaseTypes for TestLiquidityPools<AccountId> {
 	type LiquidityPoolId = LiquidityPoolId;
 	type CurrencyId = CurrencyId;
 }
+
+pub fn spread() -> Permill {
+	Permill::from_rational_approximation(5u32, 1000u32)
+}
+pub fn greedy_slippage() -> Permill {
+	Permill::from_rational_approximation(3u32, 1000u32)
+}
+pub fn tolerable_slippage() -> Permill {
+	Permill::from_rational_approximation(7u32, 1000u32)
+}
+pub fn additional_collateral_ratio() -> Permill {
+	Permill::from_percent(5)
+}
+
 impl LiquidityPoolsConfig for TestLiquidityPools<AccountId> {
 	fn get_bid_spread(_pool_id: Self::LiquidityPoolId, _currency_id: Self::CurrencyId) -> Permill {
-		Permill::from_percent(3)
+		spread()
 	}
 
 	fn get_ask_spread(_pool_id: Self::LiquidityPoolId, _currency_id: Self::CurrencyId) -> Permill {
-		Permill::from_percent(3)
+		spread()
 	}
 
 	fn get_additional_collateral_ratio(_pool_id: Self::LiquidityPoolId, _currency_id: Self::CurrencyId) -> Permill {
-		Permill::from_percent(3)
+		additional_collateral_ratio()
 	}
 }
+
 impl LiquidityPoolsCurrency<AccountId> for TestLiquidityPools<AccountId> {
 	type Balance = Balance;
 	type Error = &'static str;
@@ -190,15 +241,57 @@ impl Trait for Runtime {
 	type BalanceToPrice = BalancePriceConverter;
 	type PriceToBalance = BalancePriceConverter;
 }
+pub type SyntheticProtocol = Module<Runtime>;
 
-#[derive(Default)]
-pub struct ExtBuilder;
+const ALICE_ACC_ID: AccountId = 0;
+pub fn alice() -> Origin {
+	Origin::signed(ALICE_ACC_ID)
+}
+
+pub struct ExtBuilder {
+	currency_id: CurrencyId,
+	endowed_accounts: Vec<AccountId>,
+	initial_balance: Balance,
+}
+
+impl Default for ExtBuilder {
+	fn default() -> Self {
+		Self {
+			currency_id: CurrencyId::AUSD,
+			endowed_accounts: vec![0],
+			initial_balance: 0,
+		}
+	}
+}
 
 impl ExtBuilder {
-	pub fn build(self) -> runtime_io::TestExternalities {
-		frame_system::GenesisConfig::default()
+	pub fn balances(mut self, account_ids: Vec<AccountId>, initial_balance: Balance) -> Self {
+		self.endowed_accounts = account_ids;
+		self.initial_balance = initial_balance;
+		self
+	}
+
+	pub fn one_hundred_usd_for_alice(self) -> Self {
+		self.balances(vec![ALICE_ACC_ID], 100)
+	}
+
+	pub fn build_and_reset_env(self) -> runtime_io::TestExternalities {
+		unsafe {
+			MOCK_PRICE_SOURCE.set_default();
+		}
+
+		let mut t = frame_system::GenesisConfig::default()
 			.build_storage::<Runtime>()
-			.unwrap()
-			.into()
+			.unwrap();
+
+		orml_tokens::GenesisConfig::<Runtime> {
+			tokens: vec![self.currency_id],
+			initial_balance: self.initial_balance,
+			endowed_accounts: self.endowed_accounts,
+		}
+		.assimilate_storage(&mut t)
+		.unwrap();
+
+		t.into()
 	}
 }
