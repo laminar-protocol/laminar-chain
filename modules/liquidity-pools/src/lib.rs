b627784c7bcf8323e8500cf6ef369a65d7c5378a
Ermal Kaleci <ermalkaleci@gmail.com>
2020-03-11 11:15:38 +0100

    deposit_liquidity checks pool exists

--- modules/liquidity-pools/src/lib.rs
+++ modules/liquidity-pools/src/lib.rs
@@ -224,6 +224,7 @@ decl_error! {
 		CannotWithdrawExistentialDeposit,
 		SwapRateTooHigh,
 		SpreadTooHigh,
+		PoolNotExists,
 	}
 }
 
@@ -364,6 +365,7 @@ impl<T: Trait> Module<T> {
 	}
 
 	fn _deposit_liquidity(who: &T::AccountId, pool_id: T::LiquidityPoolId, amount: Balance) -> DispatchResult {
+		ensure!(<Owners<T>>::contains_key(&pool_id), Error::<T>::PoolNotExists);
 		let balance = Self::balances(&pool_id);
 		let new_balance = balance.checked_add(amount).ok_or(Error::<T>::CannotDepositAmount)?;
 		// transfer amount to this pool
