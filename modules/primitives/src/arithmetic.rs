fa90c9657bd96a0c382474a98dd3b69a13afeea1
Shaopeng Wang <spxwang@gmail.com>
2020-03-18 16:49:16 +1300

    Open position (#106)
    * Update ask_price and bid_price fn.
    
    * Impl open_position.
    
    * Renaming.
    
    * Update orml.
    
    * Open position unit tests.
    
    * Open position failure cases unit tests.
    
    * Open short position unit test.
    
    * Fix typo & clean up.

--- modules/primitives/src/arithmetic.rs
+++ modules/primitives/src/arithmetic.rs
@@ -9,6 +9,15 @@ pub fn fixed_128_from_fixed_u128(f: FixedU128) -> Fixed128 {
 	Fixed128::from_parts(parts)
 }
 
+pub fn fixed_128_mul_signum(f: Fixed128, signum: i128) -> Fixed128 {
+	Fixed128::from_parts(f.deconstruct().saturating_mul(signum))
+}
+
 pub fn fixed_128_from_u128(u: u128) -> Fixed128 {
 	Fixed128::from_parts(u.unique_saturated_into())
+}
+
+/// Create a `u128` from `Fixed128` by saturating.
+pub fn u128_from_fixed_128(f: Fixed128) -> u128 {
+	f.deconstruct().unique_saturated_into()
 }
