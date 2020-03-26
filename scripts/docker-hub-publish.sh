e0b9f89e42075be1013cbcbace0fb8f90dee6bcd
Shaopeng Wang <spxwang@gmail.com>
2020-03-26 23:05:41 +1300

    Add docker hub dev publish script.

--- scripts/docker-hub-publish.sh
+++ scripts/docker-hub-publish.sh
@@ -7,6 +7,6 @@ if [[ -z "$1" ]] ; then
     exit 1
 fi
 
-docker build . -t laminardev/laminar-node:$1 -t laminardev/laminar-node:latest --no-cache
-docker push laminardev/laminar-node:$1
-docker push laminardev/laminar-node:latest
+docker build . -t laminar/laminar-node:$1 -t laminar/laminar-node:latest --no-cache
+docker push laminar/laminar-node:$1
+docker push laminar/laminar-node:latest
