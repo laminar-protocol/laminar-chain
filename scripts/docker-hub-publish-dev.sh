d2c2d3c2483c6dc61270adc0c956416596f2f2ad
Shaopeng Wang <spxwang@gmail.com>
2020-04-06 15:10:01 +1200

    Script for docker: build and push the latest tagged.

--- scripts/docker-hub-publish-dev.sh
+++ scripts/docker-hub-publish-dev.sh
@@ -2,5 +2,6 @@
 
 VERSION=$(git rev-parse --short HEAD)
 
-docker build . -t laminardev/laminar-node:$VERSION --no-cache
+docker build . -t laminardev/laminar-node:$VERSION -t laminardev/laminar-node:latest --no-cache
 docker push laminardev/laminar-node:$VERSION
+docker push laminardev/laminar-node:latest
