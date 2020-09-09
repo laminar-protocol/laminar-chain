3e0ca8542af5decd649ff25d550dffd340a92b2d
Shaopeng Wang <spxwang@gmail.com>
2020-09-10 10:08:46 +1200

    Get ParaId from parachain_info module.

--- runtime/dev/src/lib.rs
+++ runtime/dev/src/lib.rs
@@ -811,7 +811,6 @@ impl parachain_info::Trait for Runtime {}
 
 parameter_types! {
 	pub const RelayChainCurrencyId: CurrencyId = CurrencyId::DOT;
-	pub GetParaId: ParaId = 5001.into();
 }
 
 pub struct RelayToNative;
@@ -843,7 +842,7 @@ impl orml_xtokens::Trait for Runtime {
 	type FromRelayChainBalance = RelayToNative;
 	type ToRelayChainBalance = NativeToRelay;
 	type UpwardMessage = cumulus_upward_message::RococoUpwardMessage;
-	type ParaId = GetParaId;
+	type ParaId = ParachainInfo;
 }
 
 construct_runtime!(
